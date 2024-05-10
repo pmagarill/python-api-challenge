a358fe890d2144d2051551e15271c1e9e7c50ce1
Paul M <paul.magarill@gmail.com>
2024-05-09 20:42:43 -0700

    final submission

---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "2f2d7c449f6863706c06f9df5e67f5e8"
+
+# Geoapify API Key
+geoapify_key = "f5bd92337b734143b8a60bebb79a0152"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2f2d7c449f6863706c06f9df5e67f5e8"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "02128272095d4cd19515b13c62e8172d"
